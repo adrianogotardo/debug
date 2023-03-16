f93544a0820a2637cd9ccba7ff3e81b7675b04ae
Adriano <adrianogotardo.12@gmail.com>
2023-03-16 12:15:06 -0300

    removed the '/historico' route

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ export default function App() {
             <Route path='/cadastro' element={<SignUpPage />} />
             <Route path='/hoje' element={<TodayPage />} />
             <Route path='/habitos' element={<HabitsPage />} />
-            <Route path='/historico' element={<HistoryPage />} />
           </Routes>
           <Menu />
         </BrowserRouter>
